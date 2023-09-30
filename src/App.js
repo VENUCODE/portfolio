a733c8471cc59c6c7a18f41214b5d12f801ef2db
venu <yedukondalunaik12345@gmail.com>
2023-09-30 22:21:42 +0530

    installed tsparticles,react-animate-site,vertical-timeline,eslint,prettier,router-dom and setup the intial router for the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
+import {Routes,Route} from "react-router-dom"
+import Skills from "./container/skills/Skills";
+import Resume from "./container/resume/Resume";
+import Home from "./container/home/Home";
 
+import About from "./container/about/About";
+import Portfolio from "./container/portfolio/Portfolio";
+import NotFound from "./container/NotFound";
 function App() {
   return (
      <div>
-      Hello
+      {/* ts particles */}
+      {/* navbar */}
+      {/* main page */}
+      
+      <Routes>
+        <Route index path='/' element={<Home/>}/>
+        <Route  path='/home' element={<Home/>}/>
+        <Route  path='/about' element={<About/>}/>
+        <Route  path='/skills' element={<Skills/>}/>
+        <Route  path='/resume' element={<Resume/>}/>
+        <Route  path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
      </div>
   );
-}
+} 
 
 export default App;
