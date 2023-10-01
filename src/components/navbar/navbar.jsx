cfebdcd537e311bbd201fb8fa6f1b05350b45cc3
venu <yedukondalunaik12345@gmail.com>
2023-10-01 13:00:23 +0530

    added the navbar styles and toggler functionality and body background image

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -24,12 +24,12 @@ const Navbar = () => {
 
   const renderLinks = (links) => {
     return (
-      <ul className="flex p-2 flex-row max-md:hidden relative items-center gap-[15px] ">
+<ul className={`nav-menu md:flex md:flex-row   max-md:absolute max-md:top-[80px] max-md: ${navToggle ? 'right-[0%]' : 'right-[-100%]'} max-md: ${navToggle ? 'flex-column' : 'hidden'}`}>
         {links.map((item) => (
-          <li className="flex transition-all hover:bg-slate-700 p-5 hover:border-b-2  gap-[4px] items-center flex-row " key={item.label}>
+          <li className="flex max-md:rounded-lg hover:from-blue-500 p-4 max-md:p-3 max-md:hover:bg-gradient-to-l from-blue-800 max-md:my-2 max-md:mx-3  hover:text-blue-100  gap-[4px] items-center flex-row " key={item.label}>
             <item.icon className="h-[10px]"size={20} color="white" />{" "}
             {/* Render the icon component directly */}
-            <Link  className=" text-2xl max-md:text-lg text-sky-100"to={item.to || item.path}>{item.label}</Link>{" "}
+            <Link  className=" md:text-xl max-md:text-lg text-blue-200"to={item.to || item.path}>{item.label}</Link>{" "}
             {/* Render the Link with label */}
           </li>
         ))}
@@ -41,18 +41,18 @@ const Navbar = () => {
     setNavToggle(!navToggle);
   };
   return (
-    <nav className="   flex flex-row  h-[80px] bg-slate-400 justify-between   items-center px-[10px] py-2">
+    <nav className=" sticky top-0 relative flex flex-row  bg-gradient-to-l from-blue-800  h-[80px] bg--400 justify-between   items-center px-[10px] py-2">
       <div className="navbar-icon px-2">
         <Link to="/">
-          <FaCode size={50} color="cyan" />
+          <FaCode size={30} color="cyan" />
         </Link>
       </div>
       <div className="">{renderLinks(links)}</div>
       <div className="  hidden max-md:block" onClick={handleToggle}>
         {navToggle ? (
-          <FaTimes size={30} color="white" />
+          <FaTimes size={20} color="white" />
         ) : (
-          <FaBars size={30} color="white" />
+          <FaBars size={20} color="white" />
         )}
       </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
-    screens: {
-      'sm': '376px',
-      // => @media (min-width: 576px) { ... }
+    extend: {
+      backgroundImage: {
+        "home-bg": "url('/public/home.jpg')",
+      },
+      screens: {
+        sm: "376px",
+        // => @media (min-width: 576px) { ... }
 
-      'md': '999px',
-      // => @media (min-width: 960px) { ... }
+        md: "999px",
+        // => @media (min-width: 960px) { ... }
 
-      'lg': '1290px',
-      // => @media (min-width: 1440px) { ... }
-      'xl': '1440px',
-      // => @media (min-width: 1440px) { ... }
+        lg: "1290px",
+        // => @media (min-width: 1440px) { ... }
+        xl: "1440px",
+        // => @media (min-width: 1440px) { ... }
+      },
     },
   },
   plugins: [],
-}
+};
